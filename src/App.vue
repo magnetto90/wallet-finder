7a10107119b022a83e29f4783eb1f1d4573bce9d
Erik <esnetto@gmail.com>
2022-07-31 18:13:48 -0300

    ethereum support

--- src/App.vue
+++ src/App.vue
@@ -30,12 +30,13 @@
           Get Balance
         </v-btn>
         <h3>Wallet: {{wallet}}</h3>
-        <p>{{cloBalance}} WEI on <a :href="'https://explorer.callisto.network/address/'+wallet+'/transactions'" target="_blank">Callisto Network</a></p>
-        <p>{{bscBalance}} WEI on <a :href="'https://bscscan.com/address/'+wallet" target="_blank">Binance Smart Chain</a></p>
-        <p>{{plyBalance}} WEI on <a :href="'https://polygonscan.com/address/'+wallet" target="_blank">Polygon</a></p>
-        <p>{{etcBalance}} WEI on <a :href="'https://etcblockexplorer.com/address/'+wallet" target="_blank">Ethereum Classic</a></p>
-        <p>{{vlsBalance}} WEI on <a :href="'https://evmexplorer.velas.com/'+wallet+'/transactions'" target="_blank">Velas</a></p>
-        <p>{{avaBalance}} WEI on <a :href="'https://avascan.info/blockchain/c/address/'+wallet" target="_blank">Avalanche</a></p>
+        <p>{{cloBalance}} WEI on <a :href="'https://explorer.callisto.network/address/'+wallet+'/transactions'" target="_blank">Callisto Network</a> (Nonce: {{cloNonce}})</p>
+        <p>{{ethBalance}} WEI on <a :href="'https://etherscan.io/address/'+wallet" target="_blank">Ethereum</a> (Nonce: {{ethNonce}})</p>        
+        <p>{{bscBalance}} WEI on <a :href="'https://bscscan.com/address/'+wallet" target="_blank">Binance Smart Chain</a> (Nonce: {{bscNonce}})</p>
+        <p>{{plyBalance}} WEI on <a :href="'https://polygonscan.com/address/'+wallet" target="_blank">Polygon</a> (Nonce: {{plyNonce}})</p>
+        <p>{{etcBalance}} WEI on <a :href="'https://etcblockexplorer.com/address/'+wallet" target="_blank">Ethereum Classic</a> (Nonce: {{etcNonce}})</p>
+        <p>{{vlsBalance}} WEI on <a :href="'https://evmexplorer.velas.com/'+wallet+'/transactions'" target="_blank">Velas</a> (Nonce: {{vlsNonce}})</p>
+        <p>{{avaBalance}} WEI on <a :href="'https://avascan.info/blockchain/c/address/'+wallet" target="_blank">Avalanche</a> (Nonce: {{avaNonce}})</p>
       </v-card>
     </v-container>
 
@@ -51,19 +52,28 @@ export default {
     privateKey: "0x0000000000000000000000000000000000000000000000000000000000000010",
     providerRPC: {
         clo: 'https://rpc.callisto.network/',
+        eth: 'https://rpc.ankr.com/eth',
         bsc: 'https://bsc-dataseed.binance.org/',
         ply: 'https://polygon-rpc.com/',
         etc: 'https://www.ethercluster.com/etc',
         vls: 'https://evmexplorer.velas.com/rpc',
-        ava: 'https://api.avax.network/ext/bc/C/rpc'
+        ava: 'https://api.avax.network/ext/bc/C/rpc',
     },
     wallet: "",
     cloBalance: "",
+    ethBalance: "",    
     bscBalance: "",
     plyBalance: "",
     etcBalance: "",
     vlsBalance: "",
-    avaBalance: ""
+    avaBalance: "",
+    cloNonce: "",
+    ethNonce: "",
+    bscNonce: "",
+    plyNonce: "",
+    etcNonce: "",
+    vlsNonce: "",
+    avaNonce: "",
   }),
   mounted () {
     this.getBalance()
@@ -82,14 +92,23 @@ export default {
       },
     getBalance: function () {
       this.cloBalance = ""
+      this.ethBalance = ""      
       this.bscBalance = ""
       this.plyBalance = ""
       this.etcBalance = ""
       this.vlsBalance = ""
       this.avaBalance = ""
+      this.cloNonce = ""
+      this.ethNonce = ""      
+      this.bscNonce = ""
+      this.plyNonce = ""
+      this.etcNonce = ""
+      this.vlsNonce = ""
+      this.avaNonce = ""
 
       const clo = new Web3(this.providerRPC.clo);
-      const bsc = new Web3(this.providerRPC.bsc);
+      const eth = new Web3(this.providerRPC.eth);
+      const bsc = new Web3(this.providerRPC.bsc);      
       const ply = new Web3(this.providerRPC.ply);
       const etc = new Web3(this.providerRPC.etc);
       const vls = new Web3(this.providerRPC.vls);
@@ -101,27 +120,53 @@ export default {
 
       clo.eth.getBalance(account).then(balance => {
           this.cloBalance = balance
+          clo.eth.getTransactionCount(account).then(nonce => {
+            this.cloNonce = nonce
+          })
       })
+
+      eth.eth.getBalance(account).then(balance => {
+          this.ethBalance = balance
+          eth.eth.getTransactionCount(account).then(nonce => {
+            this.ethNonce = nonce
+          })
+      })      
 
       bsc.eth.getBalance(account).then(balance => {
           this.bscBalance = balance
+          bsc.eth.getTransactionCount(account).then(nonce => {
+            this.bscNonce = nonce
+          })
       })
 
       ply.eth.getBalance(account).then(balance => {
           this.plyBalance = balance
+          ply.eth.getTransactionCount(account).then(nonce => {
+            this.plyNonce = nonce
+          })
       })
 
       etc.eth.getBalance(account).then(balance => {
           this.etcBalance = balance
+          etc.eth.getTransactionCount(account).then(nonce => {
+            this.etcNonce = nonce
+          })
       })
 
       vls.eth.getBalance(account).then(balance => {
           this.vlsBalance = balance
+          vls.eth.getTransactionCount(account).then(nonce => {
+            this.vlsNonce = nonce
+          })
       })
 
       ava.eth.getBalance(account).then(balance => {
           this.avaBalance = balance
+          ava.eth.getTransactionCount(account).then(nonce => {
+            this.avaNonce = nonce
+          })
       })
+  
     }
   }
 };
